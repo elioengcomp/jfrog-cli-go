05b519ec19f8137596de9ee2c8d50fcc5b14bf06
Alexei Vainshtein <alexeiv@jfrog.com>
2019-01-21 01:30:26 +0200

    On failed graph, run tidy and a fix for an issue with reverting to go.sum of the root project

--- jfrog-cli/artifactory/commands/golang/gorecursivepublish.go
+++ jfrog-cli/artifactory/commands/golang/gorecursivepublish.go
@@ -1,7 +1,6 @@
 package golang
 
 import (
-	"fmt"
 	"github.com/jfrog/jfrog-cli-go/jfrog-cli/artifactory/utils/golang"
 	"github.com/jfrog/jfrog-cli-go/jfrog-cli/artifactory/utils/golang/project"
 	"github.com/jfrog/jfrog-cli-go/jfrog-cli/artifactory/utils/golang/project/dependencies"
@@ -19,13 +18,12 @@ func Execute(targetRepo, goModEditMessage string, details *config.ArtifactoryDet
 	if err != nil {
 		return err
 	}
-	shouldRevertMod := false
 	wd, err := os.Getwd()
 	if err != nil {
 		return rgf.revert(wd, err)
 	}
 	if !modFileExists {
-		shouldRevertMod, err = rgf.prepareModFile(wd, goModEditMessage)
+		err = rgf.prepareModFile(wd, goModEditMessage)
 		if err != nil {
 			return err
 		}
@@ -34,6 +32,16 @@ func Execute(targetRepo, goModEditMessage string, details *config.ArtifactoryDet
 		rgf.modFileContent, rgf.modFileStat, err = golang.GetFileDetails("go.mod")
 		if err != nil {
 			return err
+		}
+		rgf.shouldRevertSumFile, err = fileutils.IsFileExists("go.sum", false)
+		if err != nil {
+			return err
+		}
+		if rgf.shouldRevertSumFile {
+			rgf.sumFileContent, rgf.sumFileStat, err = golang.GetFileDetails("go.sum")
+			if err != nil {
+				return err
+			}
 		}
 	}
 
@@ -45,70 +53,115 @@ func Execute(targetRepo, goModEditMessage string, details *config.ArtifactoryDet
 	err = goProject.DownloadFromVcsAndPublish(targetRepo, "", goModEditMessage, true, false, details)
 	if err != nil {
 		if !modFileExists {
-			// Now lets revert to empty mod.
-			modContent := string(rgf.modFileContent)
-			lines := strings.Split(modContent, "\n")
-			//result := lines[0] + "\n" + lines[1] + "\n" + lines[2] + "\n"
-			mod := strings.Join(lines[:3], "\n")
-			fmt.Println(mod)
-			rgf.modFileContent = []byte(mod)
+			log.Debug("Graph failed, preparing to run go mod tidy on the root project since got the following error:", err.Error())
+			err = rgf.prepareAndRunTidyOnFailedGraph(wd, goProject, targetRepo, goModEditMessage, details)
+			if err != nil {
+				return rgf.revert(wd, err)
+			}
+		} else {
+			return rgf.revert(wd, err)
 		}
-		return rgf.revert(wd, err)
 	}
-	if shouldRevertMod {
+
+	if rgf.shouldRevertModFile || rgf.shouldRevertSumFile {
 		err = os.Chdir(wd)
 		if err != nil {
 			return rgf.revert(wd, err)
 		}
+		return rgf.revert(wd, nil)
+	}
+	return nil
+}
+
+func (rgf *rootGoFiles) prepareAndRunTidyOnFailedGraph(wd string, goProject project.Go, targetRepo, goModEditMessage string, details *config.ArtifactoryDetails) error {
+	// First revert the mod to an empty mod that includes only module name
+	lines := strings.Split(string(rgf.modFileContent), "\n")
+	emptyMod := strings.Join(lines[:3], "\n")
+	rgf.modFileContent = []byte(emptyMod)
+	rgf.shouldRevertModFile = true
+	err := rgf.revert(wd, nil)
+	if err != nil {
+		log.Error(err)
+	}
+	// Run go mod tidy.
+	err = golang.RunGoModTidy()
+	if err != nil {
+		return err
+	}
+	// Perform collection again after tidy finished successfully.
+	err = goProject.DownloadFromVcsAndPublish(targetRepo, "", goModEditMessage, true, false, details)
+	if err != nil {
 		return rgf.revert(wd, err)
+	}
+	return nil
+}
+
+func (rgf *rootGoFiles) revert(wd string, err error) error {
+	if rgf.shouldRevertModFile {
+		log.Debug("Reverting to original go.mod of the root project", )
+		revertErr := ioutil.WriteFile("go.mod", rgf.modFileContent, rgf.modFileStat.Mode())
+		if revertErr != nil {
+			if err != nil {
+				log.Error(revertErr)
+				return err
+			} else {
+				return revertErr
+			}
+		}
+	}
+
+
+	if rgf.shouldRevertSumFile {
+		log.Debug("Reverting to original go.sum of the root project", )
+		revertErr := ioutil.WriteFile("go.sum", rgf.sumFileContent, rgf.sumFileStat.Mode())
+		if revertErr != nil {
+			if err != nil {
+				log.Error(revertErr)
+				return err
+			} else {
+				return revertErr
+			}
+		}
+	}
+	return nil
+}
+
+func (rgf *rootGoFiles) prepareModFile(wd, goModEditMessage string) error {
+	err := golang.RunGoModInit("", goModEditMessage)
+	if err != nil {
+		return err
+	}
+	regExp, err := dependencies.GetRegex()
+	if err != nil {
+		return err
+	}
+	notEmptyModRegex := regExp.GetNotEmptyModRegex()
+	rgf.modFileContent, rgf.modFileStat, err = golang.GetFileDetails("go.mod")
+	if err != nil {
+		return err
+	}
+	projectPackage := dependencies.Package{}
+	projectPackage.SetModContent(rgf.modFileContent)
+	packageWithDep := dependencies.PackageWithDeps{Dependency: &projectPackage}
+	if !packageWithDep.PatternMatched(notEmptyModRegex) {
+		log.Debug("Root mod is empty, preparing to run 'go mod tidy'")
+		err = golang.RunGoModTidy()
+		if err != nil {
+			return rgf.revert(wd, err)
+		}
+		rgf.shouldRevertModFile = true
+	} else {
+		log.Debug("Root project mod not empty.")
 	}
 
 	return nil
 }
 
-func (rgf *rootGoFiles) revert(wd string, err error) error {
-	log.Debug("Reverting to original go.mod of the root project", )
-	revertErr := ioutil.WriteFile("go.mod", rgf.modFileContent, rgf.modFileStat.Mode())
-	if revertErr != nil && err != nil {
-		log.Error(revertErr)
-		return err
-	}
-	return revertErr
-}
-
-func (rgf *rootGoFiles) prepareModFile(wd, goModEditMessage string) (bool, error) {
-	err := golang.RunGoModInit("", goModEditMessage)
-	if err != nil {
-		return false, err
-	}
-	regExp, err := dependencies.GetRegex()
-	if err != nil {
-		return false, err
-	}
-	notEmptyModRegex := regExp.GetNotEmptyModRegex()
-	rgf.modFileContent, rgf.modFileStat, err = golang.GetFileDetails("go.mod")
-	if err != nil {
-		return false, err
-	}
-	projectPackage := dependencies.Package{}
-	projectPackage.SetModContent(rgf.modFileContent)
-	packageWithDep := dependencies.PackageWithDeps{Dependency: &projectPackage}
-	shouldRevertMod := false
-	if !packageWithDep.PatternMatched(notEmptyModRegex) {
-		log.Debug("Root mod is empty, preparing to run 'go mod tidy'")
-		err = golang.RunGoModTidy()
-		if err != nil {
-			return false, rgf.revert(wd, err)
-		}
-		shouldRevertMod = true
-	} else {
-		log.Debug("Root project mod not empty.")
-	}
-
-	return shouldRevertMod, nil
-}
-
 type rootGoFiles struct {
-	modFileContent []byte
-	modFileStat    os.FileInfo
+	modFileContent      []byte
+	modFileStat         os.FileInfo
+	shouldRevertModFile bool
+	shouldRevertSumFile bool
+	sumFileContent      []byte
+	sumFileStat         os.FileInfo
 }
